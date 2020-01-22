7302a7c5d011314cde7c54b77e30a05c111c52a5
bak83 <bak.dariusz@gmail.com>
2020-01-22 21:03:41 +0100

    poprawa

--- LiczbyGradowe/Program.cs
+++ LiczbyGradowe/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace LiczbyGradowe
 {
@@ -7,24 +10,30 @@ namespace LiczbyGradowe
 		static void Main(string[] args)
 		{
 			int n = 0;
+			List<int> list = new List<int>();
 			Console.WriteLine("Podaj liczbę");
 			n = int.Parse(Console.ReadLine());
-
+			list.Add(n);
 			while (n != 1)
 			{
 				if (n % 2 == 0)
 				{
 					n /= 2;
+					list.Add(n);
 				}
 
 				else
 				{
 					n = 3 * n + 1;
+					list.Add(n);
+
 				}
-			Console.WriteLine(n);
+				Console.WriteLine(n);
+
 			}
-			
-			
+			Console.WriteLine("Maksymalna liczba: " + list.Max());
+			Console.WriteLine("Ilość ktoków: " + list.Count);
+
 		}
 	}
 }
